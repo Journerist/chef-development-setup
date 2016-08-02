2d654c81bf9db0e74b3aee2af1e17dfbcd70dc90
journerist <akrillo89@gmail.com>
2016-08-02 22:40:00 +0200

    add sublime preferences and hotkeys

--- cookbooks/sublime/recipes/default.rb
+++ cookbooks/sublime/recipes/default.rb
@@ -9,12 +9,12 @@
 
 currentUser = `whoami`.chomp
 
-# download package controle
+# download package control
 remote_file "/Users/#{currentUser}/Library/Application Support/Sublime Text 3/Installed Packages/Package Control.sublime-package" do
   source 'https://packagecontrol.io/Package%20Control.sublime-package'
   owner "#{currentUser}"
   group 'staff'
-  mode '0755'
+  mode '0644'
   action :create
 end
 
@@ -22,13 +22,32 @@ puts Dir.pwd
 
 # add packages to install using package control
 # FIXME: in chef-client 12.12 whitespaces in paths doesn't work. Until it is fixed the whitespace is removed from
-# the filename. Sadly we need to wait to create a clonable repo for sublime users without mentioning about chef because
-# this file name differs from the package controle filename.
+# the filename. Sadly we need to wait to create a clonable repository for sublime users without mentioning about chef because
+# this file name differs from the package control filename.
 cookbook_file "/Users/#{currentUser}/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings" do
   source "default/PackageControl.sublime-settings"
   owner "#{currentUser}"
   group 'staff'
-  mode '0755'
+  mode '0644'
+  action :create
+end
+
+# user settings
+cookbook_file "/Users/#{currentUser}/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" do
+  source "default/Preferences.sublime-settings"
+  owner "#{currentUser}"
+  group 'staff'
+  mode '0644'
+  action :create
+end
+
+# hotkeys
+# FIXME: no whitespace support in 12.12. Rename file when fixed to match to real filename.
+cookbook_file "/Users/#{currentUser}/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap" do
+  source "default/Default(OSX).sublime-keymap"
+  owner "#{currentUser}"
+  group 'staff'
+  mode '0644'
   action :create
 end
 
