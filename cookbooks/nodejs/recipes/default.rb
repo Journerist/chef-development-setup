2642cbb9f65e979666b8ac281c50e4333ac08877
journerist <akrillo89@gmail.com>
2016-08-06 19:03:54 +0200

    add nodejs

---
+++ cookbooks/nodejs/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: nodejs
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+currentUser = `whoami`.chomp
+
+remote_file "/Users/#{currentUser}/Downloads/node-v4.4.7.pkg" do
+  source 'https://nodejs.org/dist/v4.4.7/node-v4.4.7.pkg'
+  owner "#{currentUser}"
+  group 'staff'
+  mode '0755'
+  action :create
+end
+
+execute "install nodejs" do
+  command "installer -pkg /Users/#{currentUser}/Downloads/node-v4.4.7.pkg -target /"
+  user "root"
+  action :nothing
+end
