b9204739dabc900bfe00c7e436b787e243cc9db9
journerist <akrillo89@gmail.com>
2016-08-06 17:50:26 +0200

    add kaleidoscope for merging and diff

---
+++ cookbooks/kaleidoscope/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: kaleidoscope
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+zip_app_package "Kaleidoscope" do
+  source  "http://cdn.kaleidoscopeapp.com/releases/Kaleidoscope-2.1.1-219.zip"
+end
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 knife supermarket install dmg
-chef-client -z -o recipe['binaryLinks'],recipe['mas'],recipe['sublime'],recipe['dmg'],recipe['docker']
+chef-client -z -o recipe['binaryLinks'],recipe['mas'],recipe['sublime'],recipe['dmg'],recipe['docker'],recipe['zip_app'],recipe['kaleidoscope']
