15a95a2783b139d13cffa2af4c0166dbb327a8d7
journerist <akrillo89@gmail.com>
2016-08-06 18:16:13 +0200

    add git

---
+++ cookbooks/git/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: git
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+currentUser = `whoami`.chomp
+
+# set rights to create a link, otherwise there will be a linking error when installing git
+execute "chown-usr-local" do
+  command "chown -R #{currentUser}:staff /usr/local"
+  user "root"
+  action :nothing
+end
+
+package 'Install Git' do
+    package_name 'git'
+end
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 knife supermarket install dmg
-chef-client -z -o recipe['binaryLinks'],recipe['mas'],recipe['sublime'],recipe['dmg'],recipe['docker'],recipe['zip_app'],recipe['kaleidoscope']
+chef-client -z -o recipe['binaryLinks'],recipe['mas'],recipe['git'],recipe['sublime'],recipe['dmg'],recipe['docker'],recipe['zip_app'],recipe['kaleidoscope']
