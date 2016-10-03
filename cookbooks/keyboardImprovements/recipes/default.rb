3ef33c43d6f83e89861a18d66c5fdf909959c8db
journerist <journerist@journerists-MBP.fritz.box>
2016-10-03 19:13:30 +0200

    lower typing delays

--- cookbooks/keyboardImprovements/recipes/default.rb
+++ cookbooks/keyboardImprovements/recipes/default.rb
@@ -12,11 +12,11 @@ execute "enable-fn-keys" do
 end
 
 execute "lower-key-pressed-interval-1" do
-  command "defaults write -g InitialKeyRepeat -int 10"
+  command "defaults write -g InitialKeyRepeat -int 15"
   user "root"
 end
 
 execute "lower-key-pressed-interval-2" do
-  command "defaults write -g KeyRepeat -int 1"
+  command "defaults write -g KeyRepeat -int 2"
   user "root"
 end
